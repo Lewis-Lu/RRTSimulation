cdc6d47246e83c99ccfc3684418e81047288ace7
lewis <luh.lewis@gmail.com>
2019-03-26 23:12:26 +0800

    classic RRT modified

---
+++ RRT/Convertjpg2mat.m
@@ -0,0 +1,9 @@
+function Convertjpg2mat
+    prompt = 'Please Input JPG name:';
+    jpgname = input(prompt, 's');
+    whos filename
+    raw = imread(jpgname);
+    mat = double(imbinarize(rgb2gray(raw),0.5));
+    save test.mat mat
+    load test.mat
+end
--- RRT/TestBench.m
+++ RRT/TestBench.m
@@ -1,11 +1,32 @@
-clear;
 clc;
 
+k = 10000;
+
 % q_start = [80, 70]; 
-% q_goal = [707, 615];
+%% Convert .jpg file to .mat file
+% Usage: input JPG name, convert it to test.mat in workspace
+% Convertjpg2mat
+%
 
-function [vertices, edges, path] = RRT(map, q_start, q_goal, k, delta_q, p)
+%% Using converted mat
+map = load('maze.mat');
+map = map.map;
+q_start = [80, 70]; 
+q_goal = [707, 615];
+
+%% Set Parameters
+p = 0.8;
+[mapHeight, mapWidth] = size(map);
+
+for iter= 1:1:k
     
+    if(QGoalSelectionProbability > p)
+        % generate q_rand
+        q_rand = QRandGeneration(mapHeight, mapWidth);
+        
+    else
+        % q_goal as q_rand
+        q_rand = q_goal;
+        
+    end
 end
-
-
